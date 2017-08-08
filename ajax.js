fd4c9f131b428b657ef1163f095c523bc0568c6a
Harpua95 <sci102801@gmail.com>
2017-08-07 23:39:46 -0400

    updates

---
+++ ajax.js
@@ -0,0 +1,13 @@
+//build button
+//connect button
+//show GIF
+//start/stop gif
+//add more buttons to finish
+//create CSS
+
+//<div id="teamButton">
+//<form id="team-form">
+//<label for="team-input">Add a Team Name</label>
+//<input id="addTeams" type="submit" value="Submit">
+//<div id="teams">
+
